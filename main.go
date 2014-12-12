dca87c412558d9de3dcf61ed0dfbb63d31330745
Andrew Downing <ad510n1@gmail.com>
2014-12-11 17:04:27 -0800

    compactify more

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
-
-import (
+import(
 	"fmt"
 	"github.com/ad510/anylisp/core"
 	"io/ioutil"
 	"os"
 )
-
-func main() {
-	anylisp.Assert(len(os.Args) >= 2, "Usage: anylisp program.any [args]")
-	file, err := ioutil.ReadFile(os.Args[1])
-	anylisp.Assert(err == nil, "'"+os.Args[1]+"' not found")
+func main(){
+	anylisp.Assert(len(os.Args)>=2,"Usage: anylisp program.any [args]")
+	file,err:=ioutil.ReadFile(os.Args[1])
+	anylisp.Assert(err==nil,"'"+os.Args[1]+"' not found")
 	anylisp.Init()
 	anylisp.Parse(string(file))
 	anylisp.PrintTree(anylisp.TempRoot)
